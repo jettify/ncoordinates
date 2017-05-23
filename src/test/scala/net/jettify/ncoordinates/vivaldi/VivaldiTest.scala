b4bc9ad6106e8b5a2787b12c0f17db0f1a992c86
Nickolai Novik <nickolainovik@gmail.com>
2017-05-23 22:11:24 +0300

    start work on testing

---
+++ src/test/scala/net/jettify/ncoordinates/vivaldi/VivaldiTest.scala
@@ -0,0 +1,24 @@
+package net.jettify.ncoordinates.vivaldi
+
+import net.jettify.ncoordinates.NCoordinatesTestSuite
+
+class VivaldiTest extends NCoordinatesTestSuite {
+
+  test("Instance creation") {
+    val config = new ConfigBuilder()
+      .withDimensionality(0)
+      .build()
+    intercept[RuntimeException] {
+      new Vivaldi(config)
+    }
+  }
+
+  test("Instance creation") {
+    val config = new ConfigBuilder()
+      .withDimensionality(0)
+      .build()
+    intercept[RuntimeException] {
+      new Vivaldi(config)
+    }
+  }
+}
